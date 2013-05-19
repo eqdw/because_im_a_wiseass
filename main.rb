baec713b794b1a8459760f877e74b290e0fc9168
Tim Herd <rz@eqdw.net>
2013-05-19 14:27:43 -0700

    blah

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+WORDS = {
+  "foo"  => ["foo" , "bar" ],
+  "fizz" => ["fizz", "buzz"],
+  "ping" => ["ping", "pong"],
+  "herp" => ["herp", "derp"]
+}
+
+get '/' do
+  delimiter = (params["curl"] ? "\n" : "<br />")
+  words = WORDS[ (params["words"] or "fizz") ]
+  (1..100).map do |i|
+    if i % 15 == 0 
+      words.join('')
+    elsif i % 3 == 0
+      words[0]
+    elsif i % 5 == 0
+      words[1]
+    else
+      i
+    end
+  end.join(delimiter)
+end
